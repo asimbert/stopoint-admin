b62cd386daa5a20c86fe6e75a536c563393766d8
binayrs <binay.rs@gmail.com>
2017-02-14 11:23:35 +0530

    Committing admin files

---
+++ inc/config.php
@@ -0,0 +1,18 @@
+<?php
+
+define('MYSQL_HOST', 'localhost');
+define('MYSQL_USER', 'stopoint_user');
+define('MYSQL_PASS', 'l[[k5kz#)Txn');
+define('MYSQL_DB',   'stopoint_db');
+
+define('DOCUMENT_ROOT','/home/stopoint/public_html/admin/');
+define('IMAGES_PATH','/home/stopoint/public_html/images/testimonials/');
+define('PRODUCT_IMAGES','/home/stopoint/public_html/images/products/');
+define('VIDEO_IMAGES','/home/stopoint/public_html/images/video_thumbs/');
+define('VIDEO_FOLDER','/home/stopoint/public_html/videos/');
+define('VIDEO_FOLDER_URL','http://leadersedgetraining.com/videos/');
+
+$base_url = 'https://www.stopoint.com/admin';
+$site_url1 = 'https://www.stopoint.com';
+?>
+
